2d62c34f728e3bef2e8b30acb1585d5512281b4b
sanyam <sanyamkapoor001@gmail.com>
2023-04-24 17:40:23 +0530

    Added App.js

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server Running....');
+});
+
+app.listen(5000, () => {
+    console.log('Server Running on port:',5000);
+})
