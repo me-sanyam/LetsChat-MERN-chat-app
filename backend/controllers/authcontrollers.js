a0b7b09567ee4499de524edea27a21ba9722f1ae
sanyam <sanyamkapoor001@gmail.com>
2023-04-26 14:53:25 +0530

    Updated authcontrollers

--- backend/controllers/authcontrollers.js
+++ backend/controllers/authcontrollers.js
@@ -55,3 +55,18 @@ exports.login = asyncHandler(async (req, res) => {
         token: generatetoken(user._id)
     })
 })
+
+exports.getallusers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search ?
+        {
+            $or: [
+                { name: { $regex: req.query.search, $options: "i" } },
+                { email: { $regex: req.query.search, $options: "i" } }
+            ]
+        } :
+        {}
+    const users = await USER.find(keyword).find({ _id: { $ne: req.user._id } });
+    res.json({
+        users
+    })
+})
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login } = require('../controllers/authcontrollers');
+const { register, login, getallusers } = require('../controllers/authcontrollers');
+const { authenticated } = require('../middleware/authentication');
 
+router.route('/').get(authenticated, getallusers);
 router.route('/signup').post(register);
 router.route('/login').post(login);
 
